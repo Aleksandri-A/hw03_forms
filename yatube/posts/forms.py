6e415b64d04173f6bdb55d85d2992c740b9f618a
Александра Никитенко <aleksandri@cn.ru>
2023-03-11 23:17:30 +0700

    Review second

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -1,8 +1,17 @@
-from .models import Post
 from django import forms
+
+from .models import Post
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('text', 'group')
+        labels = {
+            'text': 'Текст поста',
+            'group': 'Группа',
+        }
+        help_texts = {
+            'text': 'Текст нового поста',
+            'group': 'Группа, к которой будет относиться пост',
+        }
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+
 from django.core.paginator import Paginator
 
 from django.shortcuts import render, get_object_or_404, redirect
@@ -6,23 +8,19 @@ from .models import Post, Group, User
 
 from .forms import PostForm
 
-from django.contrib.auth.decorators import login_required
-
 
 COUNT = 10
 
 
 def group_list(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')
-    paginator = Paginator(posts, COUNT)
+    paginator = Paginator(group.posts.order_by('-pub_date'), COUNT)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     title = 'Записи группы: ' + str(group)
     context = {
         'page_obj': page_obj,
         'group': group,
-        'posts': posts,
         'title': title,
         'h1': group.title,
         'description': group.description,
@@ -48,7 +46,6 @@ def profile(request, username):
     # Здесь код запроса к модели и создание словаря контекста
     user = get_object_or_404(User, username=username)
     post_list = Post.objects.filter(author=user).order_by('-pub_date')
-    post_count = Post.objects.filter(author=user).count
     paginator = Paginator(post_list, COUNT)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -57,7 +54,7 @@ def profile(request, username):
         'h1': 'Все посты пользователя ' + str(user.get_full_name()),
         'title': 'Профайл пользователя ' + str(user.get_full_name()),
         'page_obj': page_obj,
-        'h3': post_count,
+        'h3': post_list.count,
         'posts': post_list,
     }
     return render(request, 'posts/profile.html', context)
@@ -76,7 +73,7 @@ def post_detail(request, post_id):
 
 @login_required
 def post_create(request):
-    form = PostForm(request.POST)
+    form = PostForm(request.POST or None)
     template = 'posts/create_post.html'
     groups = Group.objects.all().order_by('title')
     context = {
@@ -84,14 +81,23 @@ def post_create(request):
         'form': form,
         'groups': groups,
     }
-    if request.method == 'POST':
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('posts:profile', (request.user.username))
+    if request.method != 'POST':
         return render(request, template, context)
-    return render(request, template, context)
+    if not form.is_valid():
+        return render(request, template, context)
+    post = form.save(commit=False)
+    post.author = request.user
+    post.save()
+    return redirect('posts:profile', (request.user.username))
+
+    # if request.method == 'POST':
+    #     if form.is_valid():
+    #         post = form.save(commit=False)
+    #         post.author = request.user
+    #         post.save()
+    #         return redirect('posts:profile', (request.user.username))
+    #     return render(request, template, context)
+    # return render(request, template, context)
 
 
 @login_required
@@ -108,12 +114,21 @@ def post_edit(request, post_id):
         'post': post,
         'groups': groups,
     }
-    if post.author == request.user:
-        if request.method == 'POST':
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('posts:post_detail', (post.pk))
+    if post.author != request.user:
+        return redirect('posts:post_detail', (post.pk))
+    if request.method != 'POST':
         return render(request, template, context)
-
+    post = form.save(commit=False)
+    post.author = request.user
+    post.save()
     return redirect('posts:post_detail', (post.pk))
+
+    # if post.author == request.user:
+    #     if request.method == 'POST':
+    #         post = form.save(commit=False)
+    #         post.author = request.user
+    #         post.save()
+    #         return redirect('posts:post_detail', (post.pk))
+    #     return render(request, template, context)
+
+    # return redirect('posts:post_detail', (post.pk))
